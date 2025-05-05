64697459d3044d0ad5e736c377ce4fd130de2915
mad <pagutaad@students.nu-lipa.edu.ph>
2025-05-05 05:44:45 +0100

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var type = new Typed(".typewriter-text", {
-    strings: ["Frontend Developer", "Web Designer", "UI/UX Designer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true,
-});
